f61e1298431925f034f91fd31e7efd8c01f16fb1
Steno-95 <lezzistefano95@gmail.com>
2025-06-04 16:04:44 +0200

    Feat: add react router routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { Route, Routes } from "react-router";
+import AppLayout from "./AppLayout";
+import Home from "./pages/home/Home";
+import Destination from "./pages/destination/Destination";
+
 function App() {
-  return <div className="home-bg"></div>;
+  return (
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route index element={<Home />} />
+        <Route
+          path="crew"
+          element={<div className="crew-bg site-padding"></div>}
+        />
+        <Route path="destination" element={<Destination />} />
+        <Route
+          path="technology"
+          element={<div className="tech-bg site-padding"></div>}
+        />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
