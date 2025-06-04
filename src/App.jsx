ac4fef4647cdab9e801444d2135600f47739140c
Steno-95 <lezzistefano95@gmail.com>
2025-06-04 17:09:08 +0200

    Feat: add routing  for crew

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { Route, Routes } from "react-router";
 import AppLayout from "./AppLayout";
 import Home from "./pages/home/Home";
 import Destination from "./pages/destination/Destination";
+import Crew from "./pages/crew/Crew";
 
 function App() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
         <Route index element={<Home />} />
-        <Route
-          path="crew"
-          element={<div className="crew-bg site-padding"></div>}
-        />
+        <Route path="crew" element={<Crew />} />
         <Route path="destination" element={<Destination />} />
         <Route
           path="technology"
