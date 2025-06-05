14e56e3418a4cedf58a49542533bd025b26eeb38
Steno-95 <lezzistefano95@gmail.com>
2025-06-05 14:49:46 +0200

    Feat: add mobile navigation menu

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,9 +1,54 @@
-import { Outlet } from "react-router";
+import { useState } from "react";
+import { Link, Outlet } from "react-router";
+import useScreenSize from "./hooks/useScreenSize";
 
 function AppLayout() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const isDesktopSize = useScreenSize();
+  const contextMobileNav = {
+    openMenu: () => setMobileMenuOpen(true),
+    isOpen: mobileMenuOpen,
+  };
   return (
     <main>
-      <Outlet />
+      <Outlet context={contextMobileNav} />
+      {mobileMenuOpen && !isDesktopSize && (
+        <nav className="fixed right-0 top-0 min-h-screen flex flex-col backdrop-blur-md w-[70%] py-8 px-6 gap-20">
+          <button
+            className="self-end cursor-pointer mr-2"
+            onClick={() => setMobileMenuOpen(false)}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21">
+              <g fill="#D0D6F9" fillRule="evenodd">
+                <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z" />
+                <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z" />
+              </g>
+            </svg>
+          </button>
+          <ul className="flex flex-col gap-5 tracking-widest">
+            <li className="text-white/70 uppercase backdrop-contast-200">
+              <Link to={"/"}>
+                <span className="text-white mr-3">00</span>home
+              </Link>
+            </li>
+            <li className="text-white/70 uppercase backdrop-contast-200">
+              <Link to={"/destination"}>
+                <span className="text-white mr-3">01</span>destination
+              </Link>
+            </li>
+            <li className="text-white/70 uppercase backdrop-contast-200">
+              <Link to={"/crew"}>
+                <span className="text-white mr-3">02</span>crew
+              </Link>
+            </li>
+            <li className="text-white/70 uppercase backdrop-contast-200">
+              <Link to={"/technology"}>
+                <span className="text-white mr-3">03</span>technology
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </main>
   );
 }
--- src/ui/Navigation.jsx
+++ src/ui/Navigation.jsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router";
+import { Link, useOutletContext } from "react-router";
 import useScreenSize from "../hooks/useScreenSize";
 
 function Navigation({ style }) {
   const isDesktop = useScreenSize();
-
+  const { openMenu, isOpen } = useOutletContext();
   return (
     <nav className={"navigation " + style}>
       <figure className="size-10">
-        <img src="/shared/logo.svg" />
+        <img
+          src="/shared/logo.svg"
+          alt="website logo, blue star inside a white circle"
+        />
       </figure>
       {isDesktop && (
         <ul className=" text-(--white)/70 flex gap-5 uppercase tracking-widest font-(family-name:--fn-barlow)">
@@ -36,8 +39,12 @@ function Navigation({ style }) {
           </li>
         </ul>
       )}
-      {!isDesktop && (
-        <button className="size-10">
+      {!isDesktop && !isOpen && (
+        <button
+          className="size-10 cursor-pointer"
+          aria-label="button to open mobile navigation"
+          onClick={openMenu}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
             <g fill="#D0D6F9" fillRule="evenodd">
               <path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
