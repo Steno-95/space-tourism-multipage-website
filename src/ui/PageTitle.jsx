d091c35c95328a49a008645b3917063a839171dc
Steno-95 <lezzistefano95@gmail.com>
2025-06-09 13:47:22 +0200

    Fix: patch an introduced style bug

--- src/ui/PageTitle.jsx
+++ src/ui/PageTitle.jsx
@@ -2,13 +2,13 @@ function PageTitle({ number, title, style = "" }) {
   return (
     <h1
       className={
-        "flex gap-5 font-(family-name:--fn-barlow) mb-8 md:pl-20 " + style
+        "flex gap-5 font-(family-name:--fn-barlow) my-8 md:pl-20 " + style
       }
     >
-      <span className="text-(--blue-light)/30 font-bold tracking-[0.2em]">
+      <span className="text-(--blue-light)/30 font-bold tracking-[0.2em] text-[1.2rem] lg:text-[1.5rem]">
         {number}
       </span>
-      <span className="uppercase text-(--white) tracking-[0.1em]  font-light">
+      <span className="uppercase text-(--white) tracking-[0.1em] text-[1.2rem] lg:text-[1.5rem] font-light">
         {title}
       </span>
     </h1>
