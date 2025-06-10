82c773410ba62cffff1f85a3a9f88bd37a3b94b6
Steno-95 <lezzistefano95@gmail.com>
2025-06-10 14:16:01 +0200

    Feat: improve layout up to big screen and add hover state

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,24 +2,24 @@ import Navigation from "../../ui/Navigation";
 
 function Home() {
   return (
-    <article className="home-bg ">
+    <article className="home-bg main-bg ">
       <Navigation />
-      <main className="flex flex-col gap-25 items-center site-padding">
-        <div className="flex flex-col items-center gap-5">
-          <span className="text-(--blue-light) uppercase">
+      <main className="flex flex-col gap-25 items-center site-padding lg:max-w-[60rem] xl:max-w-[80rem] xl:mx-auto lg:min-h-[80vh] lg:flex-row lg:items-end lg:justify-between lg:px-20!">
+        <div className="flex flex-col items-center lg:items-start">
+          <span className="text-(--blue-light) uppercase tracking-[.2em] font-(family-name:--fn-barlow)">
             So, you want to travel to
           </span>
-          <h1 className="text-(--white) uppercase text-[5rem]">Space</h1>
-          <p className="text-(--blue-light) text-center text-[1.2rem] leading-8 mt-5">
+          <h1 className="text-(--white) uppercase text-[6rem] font-(family-name:--fn-bellefair) tracking-wider">
+            Space
+          </h1>
+          <p className="text-(--blue-light) text-center text-[1.2rem] leading-8 mt-5 font-(family-name:--fn-barlow) max-w-[30rem] lg:text-start">
             Let's face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we'll give you a truly out of this
             world experience!
           </p>
         </div>
-        <button className="text-clip text-[1.5rem] bg-(--white) uppercase size-45 rounded-full tracking-widest">
-          Explore
-        </button>
+        <button className="home-btn">Explore</button>
       </main>
     </article>
   );
